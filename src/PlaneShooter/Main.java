fb80057d3904609b67d09bd336c544a000af6bb6
yuzhongliang22 <yuzhongliang221@gmail.com>
2017-12-08 12:45:53 +0800

    init

---
+++ src/PlaneShooter/Main.java
@@ -0,0 +1,10 @@
+package PlaneShooter;
+
+/**
+ * Created by yuyuyzl on 2017/12/8.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
